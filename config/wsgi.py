df8e9000d0ad757b70f9c7cb10cfe76a5d3a2a17
zalavadiyarimpal <zalavadiya.rimpal@gmail.com>
2025-07-07 15:31:24 +0530

    production changes

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 
 
 application = get_wsgi_application()
